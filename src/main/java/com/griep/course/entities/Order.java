0c7bbfc3af2a5fe8438656bf7eaacabf35670dcf
Renato Griep <renato-griep@procergs.rs.gov.br>
2025-08-04 10:59:10 -0300

    OrderStatus enum

--- src/main/java/com/griep/course/entities/Order.java
+++ src/main/java/com/griep/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.griep.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,6 +28,9 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 	
+	
+	private Integer orderStatus; 
+	
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
@@ -35,9 +39,10 @@ public class Order implements Serializable {
 		
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -60,6 +65,16 @@ public class Order implements Serializable {
 
 	public User getClient() {
 		return client;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public void setClient(User client) {
